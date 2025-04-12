5524debfb75051e4e8a9bca84b8f03a733f7e0e4
Atharva Pardeshi <atharva.exe@gmail.com>
2025-04-12 10:12:34 +0530

    refactor: update deep sleep module to enter deep sleep when not in use, get wake up reason

--- src/common/deep_sleep.h
+++ src/common/deep_sleep.h
@@ -2,8 +2,19 @@
 #define DEEP_SLEEP_H
 
 #include <stdint.h>
+#include <time.h>
 
-void deep_sleep_init(uint64_t wakeup_time_ms);
-void deep_sleep_start();
+// Wake-up sources
+typedef enum {
+    WAKEUP_TIMER,
+    WAKEUP_BUTTON,
+    WAKEUP_UNDEFINED
+} wakeup_reason_t;
 
-#endif // !DEEP_SLEEP_H
+void deep_sleep_init(void);
+void deep_sleep_wakeup_at(int hour, int minute);
+void deep_sleep_start(void);
+wakeup_reason_t deep_sleep_get_wakeup_reason(void);
+void deep_sleep_configure_button_wakeup(uint32_t gpio_num);
+
+#endif // DEEP_SLEEP_H
