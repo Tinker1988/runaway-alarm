8cd2d49181e96d99bab822f3c5792a0413d09c2e
Khushi Singh <ksingh.172005@gmail.com>
2025-04-14 23:42:28 +0530

    feat: code as per pico

--- src/app/state_machine.h
+++ src/app/state_machine.h
@@ -1,20 +1,14 @@
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
 
-// System states
 typedef enum {
-  STATE_IDLE,        // System is idle, can enter configuration mode
-  STATE_SLEEPING,    // System is in deep sleep until alarm time
-  STATE_ALARM_ACTIVE // Alarm is active, device is running away
+    STATE_IDLE,
+    STATE_SLEEPING,
+    STATE_ALARM_ACTIVE
 } system_state_t;
 
-// Initialize the state machine
 void state_machine_init(void);
-
-// Run the state machine (doesn't return)
 void state_machine_run(void);
-
-// Get current state
 system_state_t state_machine_get_state(void);
 
 #endif // STATE_MACHINE_H
