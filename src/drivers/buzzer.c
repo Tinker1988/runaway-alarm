e37494877e8d43d432f543b82648114719782f38
Atharva Pardeshi <atharva.exe@gmail.com>
2025-04-19 07:50:23 +0530

    fix: changes for active buzzer, which doesnt support pwm

--- src/drivers/buzzer.c
+++ src/drivers/buzzer.c
@@ -1,38 +1,20 @@
 #include "buzzer.h"
-#include <stdint.h>
-
-#define BUZZER_CLOCK_DIV 100
 
 void buzzer_init(buzzer_t *buzzer) {
-  gpio_set_function(buzzer->pin, GPIO_FUNC_PWM);
-  buzzer->slice = pwm_gpio_to_slice_num(buzzer->pin);
-  buzzer->channel = pwm_gpio_to_channel(buzzer->pin);
-
-  // Configure PWM
-  pwm_set_clkdiv(buzzer->slice, BUZZER_CLOCK_DIV);
-  pwm_set_enabled(buzzer->slice, true);
+  // Configure the pin as an output
+  gpio_init(buzzer->pin);
+  gpio_set_dir(buzzer->pin, GPIO_OUT);
 
   // Initially off
   buzzer_off(buzzer);
 }
 
-void buzzer_on(buzzer_t *buzzer, uint frequency) {
-  // calculate PWM parameters for the desired frequency
-  uint32_t clock_freq = 125000000; // 125 MHz
-  uint32_t pwm_freq = clock_freq / BUZZER_CLOCK_DIV;
-  uint wrap = pwm_freq / frequency;
+void buzzer_on(buzzer_t *buzzer) { gpio_put(buzzer->pin, 1); }
 
-  pwm_set_wrap(buzzer->slice, wrap);
-  pwm_set_chan_level(buzzer->slice, buzzer->channel,
-                     wrap / 2); // 50% duty cycle
-}
+void buzzer_off(buzzer_t *buzzer) { gpio_put(buzzer->pin, 0); }
 
-void buzzer_off(buzzer_t *buzzer) {
-  pwm_set_chan_level(buzzer->slice, buzzer->channel, 0);
-}
-
-void buzzer_beep(buzzer_t *buzzer, uint frequency, uint duration_ms) {
-  buzzer_on(buzzer, frequency);
+void buzzer_beep(buzzer_t *buzzer, uint duration_ms) {
+  buzzer_on(buzzer);
   sleep_ms(duration_ms);
   buzzer_off(buzzer);
 }
--- src/drivers/buzzer.h
+++ src/drivers/buzzer.h
@@ -1,20 +1,16 @@
-#ifndef BUZZER_H
-#define BUZZER_H
+#ifndef ACTIVE_BUZZER_H
+#define ACTIVE_BUZZER_H
 
-#include "hardware/gpio.h"
-#include "hardware/pwm.h"
 #include "pico/stdlib.h"
 #include <sys/types.h>
 
 typedef struct {
   uint pin;
-  uint slice;
-  uint channel;
 } buzzer_t;
 
 void buzzer_init(buzzer_t *buzzer);
-void buzzer_on(buzzer_t *buzzer, uint frequency);
+void buzzer_on(buzzer_t *buzzer);
 void buzzer_off(buzzer_t *buzzer);
-void buzzer_beep(buzzer_t *buzzer, uint frequency, uint duration_ms);
+void buzzer_beep(buzzer_t *buzzer, uint duration_ms);
 
-#endif // !BUZZER_H
+#endif // !ACTIVE_BUZZER_H
