99a69fa2d29a195e242ab004813542416f7c3e3e
Atharva Pardeshi <atharva.exe@gmail.com>
2025-04-26 18:32:15 +0530

    feat: improve motion logic

--- src/app/motion_logic.c
+++ src/app/motion_logic.c
@@ -3,14 +3,17 @@
 #include "stdio.h"
 
 #define OBSTACLE_THRESHOLD_CM 20.0
+#define MEASUREMENT_TIMEOUT 30000
 
-void motion_logic_update(ultrasonic_sensor_t *left_sensor,
-                         ultrasonic_sensor_t *right_sensor, motor_t *left_motor,
+void motion_logic_update(ultrasonic_config_t *left_sensor,
+                         ultrasonic_config_t *right_sensor, motor_t *left_motor,
                          motor_t *right_motor) {
 
   // read distances
-  float left_distance = ultrasonic_read_distance_cm(left_sensor);
-  float right_distance = ultrasonic_read_distance_cm(right_sensor);
+  float left_distance =
+      ultrasonic_measure_distance_cm(left_sensor, MEASUREMENT_TIMEOUT);
+  float right_distance =
+      ultrasonic_measure_distance_cm(right_sensor, MEASUREMENT_TIMEOUT);
 
   printf("Left sensor: %.1f cm, Right sensor: %.1f cm\n", left_distance,
          right_distance);
@@ -24,15 +27,23 @@ void motion_logic_update(ultrasonic_sensor_t *left_sensor,
   if (obstacle_left && obstacle_right) {
     printf("Obstacles on both sides. Reversing.\n");
     motor_backward(left_motor, 200);
-    motor_backward(right_motor, 200);
+    sleep_ms(2000);
+    motor_forward(left_motor, 200);
+    motor_forward(right_motor, 200);
   } else if (obstacle_left) {
     printf("Obstacle on left. Turning right.\n");
     motor_forward(left_motor, 200);
     motor_backward(right_motor, 200);
+    sleep_ms(500);
+    motor_forward(left_motor, 200);
+    motor_forward(right_motor, 200);
   } else if (obstacle_right) {
     printf("Obstacle on right. Turning left.\n");
     motor_forward(right_motor, 200);
     motor_backward(left_motor, 200);
+    sleep_ms(500);
+    motor_forward(left_motor, 200);
+    motor_forward(right_motor, 200);
   } else {
     printf("Path is clear. Moving forward.\n");
     motor_forward(left_motor, 200);
--- src/app/motion_logic.h
+++ src/app/motion_logic.h
@@ -4,8 +4,8 @@
 #include "../drivers/motor_driver.h"
 #include "../drivers/ultrasonic_sensor.h"
 
-void motion_logic_update(ultrasonic_sensor_t *left_sensor,
-                         ultrasonic_sensor_t *right_sensor, motor_t *left_motor,
+void motion_logic_update(ultrasonic_config_t *left_sensor,
+                         ultrasonic_config_t *right_sensor, motor_t *left_motor,
                          motor_t *right_motor);
 
 #endif // MOTION_LOGIC_H
